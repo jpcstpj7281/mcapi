ca6e8fc5723e3702ff12205c6cd644248b773137
zhouweiming <zhouweiming@3a57ee49-f89d-46b9-a3c0-71fdb2aa5dce>
2009-11-07 12:22:06 +0000

    git-svn-id: svn://svn.code.sf.net/p/mcapi/code@34 3a57ee49-f89d-46b9-a3c0-71fdb2aa5dce

---
+++ src/mcapi/MultiCore/Event.c
@@ -0,0 +1,66 @@
+#ifndef _WIN32
+#include <pthread.h>
+
+
+typedef struct MCAPI_EVENT_st {
+    pthread_mutex_t   mutex;
+    pthread_cond_t    cond;
+} MCAPI_EVENT;
+
+HANDLE EventCreate()
+{
+    MCAPI_EVENT  *pEvent = (MCAPI_EVENT *)malloc(sizeof(MCAPI_EVENT));
+    if ( pEvent == NULL )
+    {
+        return NULL;
+    }
+    pthread_mutex_init(&(pEvent->mutex), NULL);
+    pthread_cond_init(&(pEvent->cond), NULL);
+    
+    return (HANDLE)pEvent;
+}
+
+void WaitEvent(HANDLE hEvent) 
+{
+    MCAPI_EVENT *pEvent;
+
+    pEvent = (MCAPI_EVENT *)hEvent;
+
+    pthread_mutex_lock(&(pEvent->mutex));
+    
+    pthread_cond_wait(&(pEvent->cond));
+
+    pthread_mutex_unlock(&(pEvent->mutex));
+    return;
+}
+
+void SendEvent(HANDLE hEvent)  
+{
+    MCAPI_EVENT *pEvent;
+
+    pEvent = (MCAPI_EVENT *)hEvent;
+
+    pthread_mutex_lock(&(pEvent->mutex));
+
+    pthread_cond_signal(&(pEvent->cond));
+
+    pthread_mutex_unlock(&(pEvent->mutex));
+    return;
+}
+
+void EventClose(HANDLE hEvent)
+{
+    MCAPI_EVENT *pEvent;
+
+    pEvent = (MCAPI_EVENT *)hEvent;
+
+    pthread_cond_destroy(&(pEvent->cond));
+    pthread_mutex_destroy(&(pEvent->mutex));
+
+    free(Event);
+    return;
+}
+
+
+
+#endif /* _WIN32 */
