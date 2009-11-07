b0ecce4a0f49d6d351759552174f78ccc93ca109
zhouweiming <zhouweiming@3a57ee49-f89d-46b9-a3c0-71fdb2aa5dce>
2009-11-07 12:21:23 +0000

    git-svn-id: svn://svn.code.sf.net/p/mcapi/code@33 3a57ee49-f89d-46b9-a3c0-71fdb2aa5dce

---
+++ src/mcapi/include/Event.h
@@ -0,0 +1,23 @@
+#ifndef __EVENT_H__
+#define __EVENT_H__
+
+#ifndef _WIN32 
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef EVENT  HANDLE;
+
+HANDLE EventCreate();
+void WaitEvent(HANDLE hEvent);
+void SendEvent(HANDLE hEvent); 
+void EventClose(HANDLE hEvent);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _WIN32 */
+#endif /* __EVENT_H__ */
