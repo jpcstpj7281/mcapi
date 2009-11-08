55f6069b8c45580f4fbff77b4124b9981538271f
zhouweiming <zhouweiming@3a57ee49-f89d-46b9-a3c0-71fdb2aa5dce>
2009-11-08 03:10:22 +0000

    git-svn-id: svn://svn.code.sf.net/p/mcapi/code@94 3a57ee49-f89d-46b9-a3c0-71fdb2aa5dce

--- src/mcapi/include/CWaitTaskScheduler.h
+++ src/mcapi/include/CWaitTaskScheduler.h
@@ -60,7 +60,8 @@ public:
 class CWaitTaskScheduler {
 private:
     CThreadPool     m_ThreadPool;//(TaskScheduler_StartFunc, NULL, 0);
-    CDistributedQueue<CWaitTask, CLocalQueue<CWaitTask>, CStealQueue<CWaitTask>> m_DQueue;
+    CDistributedQueue<CWaitTask, CLocalQueue<CWaitTask>, 
+        CStealQueue<CWaitTask>, CStealQueue<CWaitTask>> m_DQueue;
     THREADFUNC      m_StartFunc;  //为线程池使用的线程入口函数指针
     LONG  volatile  m_lTaskId;    //Task Id,用于判断是否唤醒对应的线程
 
